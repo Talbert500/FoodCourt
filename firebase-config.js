7086dc0247f1b7a43cf5ec431ab9a7245b557abd
Talbert Herndon <tallypvp@gmail.com>
2022-03-02 20:16:55 -0700

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,18 +1,19 @@
 import { initializeApp } from "firebase/app"
 
-import {getFirestore} from '@firebase/firestore'
-import {getDatabase} from "firebase/database"
-import {getStorage} from "firebase/storage"
-import {getAuth} from '@firebase/auth'
-import {GoogleAuthProvider} from 'firebase/auth'
+import { getFirestore } from '@firebase/firestore'
+import { getDatabase } from "firebase/database"
+import { getStorage } from "firebase/storage"
+import { getAuth } from '@firebase/auth'
+import { GoogleAuthProvider } from 'firebase/auth'
+import { apiKey, authDomain, projectId, storageBucket, messagingSenderId, appId } from './config'
 
 const firebaseConfig = {
-  apiKey:"AIzaSyBUeL6s9MCZ3nH_qnpSJXGw3Z38oiCQe-U",
-  authDomain: "restaurants-db-dee75.firebaseapp.com",
-  projectId:  "restaurants-db-dee75",
-  storageBucket: "restaurants-db-dee75.appspot.com",
-  messagingSenderId: "343568497668",
-  appId:  "1:343568497668:web:f4852a9a39cd7009bd4951"
+  apiKey: apiKey,
+  authDomain: authDomain,
+  projectId: projectId,
+  storageBucket: storageBucket,
+  messagingSenderId: messagingSenderId,
+  appId: appId
 };
 
 // Initialize Firebase
@@ -21,7 +22,7 @@ export const app = initializeApp(firebaseConfig);
 
 export const db = getFirestore(app)
 
-export const database= getDatabase(app);
+export const database = getDatabase(app);
 export const storage = getStorage(app);
 
 export const auth = getAuth(app)
